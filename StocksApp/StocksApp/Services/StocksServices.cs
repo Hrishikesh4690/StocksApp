fbc0c82c84a3d8871a8d829aee6ab9879a650340
Hrishikesh Pethkar <pethkarhrishikesh@gmail.com>
2025-06-10 09:48:36 +0530

    Added new Service and Validation helper

---
+++ StocksApp/StocksApp/Services/StocksServices.cs
@@ -0,0 +1,28 @@
+﻿using StocksApp.DTO;
+using StocksApp.ServiceContracts;
+
+namespace StocksApp.Services
+{
+    public class StocksServices : IStocksService
+    {
+        public BuyOrderResponse CreateBuyOrder(BuyOrderRequest? buyOrderRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SellOrderResponse CreateSellOrder(SellOrderRequest? sellOrderRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<BuyOrderResponse> GetBuyOrders()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<SellOrderResponse> GetSellOrders()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
